e32a40a7dc79f6c49637a32da883d096dc40dbe7
Boro23 <404slimboysavage@gmail.com>
2020-12-07 17:27:41 +0530

    first-commit

---
+++ .next/static/8Wr0qbu7U1VBZauom2oaC/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-979e9f3a1143279115bc.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-3af623c93ff60c3d6840.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
